ce03f1b83bed9550758b79788ab37703a638495e
Rena Goldsmith <rena.kg@gmail.com>
2016-09-08 02:00:10 +1200

    modify config

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      // look for real db and username on my local machine
+      database: 'dictionary',
+      user:     'username',
+      password: 'kupuhuna'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      // look for real db and username on my local machine
+      database: 'my_db',
+      user:     'username',
+      password: 'kupuhuna'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: process.env.DATABASE_URL || {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
